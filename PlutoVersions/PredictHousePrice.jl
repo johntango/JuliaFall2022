307df003c60bcf56718824be0a85671e444ae29b
digitaltango <jrw@mit.edu>
2022-11-06 07:39:59 -0500

    PlutoWorks

--- PlutoVersions/PredictHousePrice.jl
+++ PlutoVersions/PredictHousePrice.jl
@@ -4,6 +4,30 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 94802ec1-6e6b-4608-bc1e-cb959868baf5
+begin
+	import Pkg
+	Pkg.add("Plots")
+	using Plots
+end 
+
+# ╔═╡ 177fe39a-0a5e-4863-9a98-97253dae4dad
+begin
+	# House Sales Data, ETL, Build Predictive Model(linear regression), 
+	# Assess Model, Deploy Model. Training data  'house100Data.csv'
+	# R^2 = 0.75  Price explained by the indep. variables
+	Pkg.add("DataFrames")
+	Pkg.add("Missings")
+	Pkg.add("CSV")
+	Pkg.add("Statistics")
+	Pkg.add("LinearAlgebra")
+	using DataFrames
+	using CSV
+	using Missings
+	using Statistics
+	using LinearAlgebra
+end
+
 # ╔═╡ ccd33cef-a4f3-43c3-aacc-f0326d07be00
 md"""
 ### House Price Prediction Exercise - data_description.txt
@@ -21,8 +45,22 @@ Utilities — Type of utilities available
 LotConfig — Lot configuration  
 """
 
-# ╔═╡ 4f32bf65-12f8-4496-a99e-6e2bb3c0d130
-df2 = DataFrame(:A => [5, 10, 15, 20, 25], :Y => [5, 10, missing, 20, 15])
+# ╔═╡ 1df4c152-9827-4511-a778-12b16d24f1cb
+
+
+# ╔═╡ ed8f6d88-2dc7-483e-ace6-321b37ce661d
+begin
+	xx = range(1,100)
+	yy = xx.^2
+	scatter(xx,yy)
+end
+
+
+# ╔═╡ d57a171a-839c-4a7d-85aa-0ccfa12ff4ec
+begin
+	train = CSV.read("house100Data.csv", DataFrame)
+	size(train)
+end
 
 # ╔═╡ 66013ac0-97ff-45d5-a704-5467853749b5
 # extract SalePrice column
@@ -48,6 +86,9 @@ end
 #shape of train
 size(train)
 
+# ╔═╡ 4f32bf65-12f8-4496-a99e-6e2bb3c0d130
+df2 = DataFrame(:A => [5, 10, 15, 20, 25], :Y => [5, 10, missing, 20, 15])
+
 # ╔═╡ e1dd2eaa-58bb-40dc-84dc-b3f7ad5b769d
 begin
 	# count missing values in column 
@@ -70,6 +111,20 @@ begin
 	
 end
 
+# ╔═╡ 52ed110a-f713-4304-8396-956feb699a4e
+begin
+	function dropColumnsWithMissingValues(df)
+	    missingColumns = getListOfMissingColumns(df)
+	    for i in 1:length(missingColumns)
+	        select!(df, Not(Symbol(missingColumns[i])))
+	    end
+	    return df
+	end
+	#train = dropColumnsWithMissingValues(train)
+	dropColumnsWithMissingValues(train)
+	size(train)
+end
+
 # ╔═╡ 2e748081-0b02-43df-b6a0-bc369c95f51c
 begin
 	# get names of non numeric columns float and int
@@ -87,10 +142,38 @@ begin
 	
 end
 
+# ╔═╡ 72d6fb52-abfa-40cc-9610-95343d3231bd
+begin
+	# drop non numeric columns
+	function dropNonNumericColumns(df)
+	    nonNumericColumns = getNonNumericColumns(df)
+	    for i in 1:length(nonNumericColumns)
+	        select!(df, Not(Symbol(nonNumericColumns[i])))
+	    end
+	    return df
+	end
+	# train = 
+	dropNonNumericColumns(train)
+	size(train)
+	
+end
+
 # ╔═╡ f16533f2-30fb-43fc-8ee7-cccc64ce6bbb
-# get name of problem column
+begin
+	# get name of problem column
+	names(train)[33]
+	select!(train, Not(Symbol("PoolArea")))
+	size(train)
+end
 
-
+# ╔═╡ c02fd9e8-98e5-4f0e-9d49-d6bb216dcf55
+begin
+	# Build correlation matrix
+	#using Statistics
+	cor(Matrix(train))
+	# Plot heatmap of correlation matrix
+	heatmap(cor(Matrix(train)))
+end
 
 # ╔═╡ dd826df7-6a8a-4ae2-9b6a-4a2b2eebff3a
 begin
@@ -118,6 +201,7 @@ begin
 	    return topCorColumns
 	end
 	topCorColumns = getTopCorrelatedColumns(train, 5)
+	
 end
 
 # ╔═╡ 713b4fe2-7acc-4e44-b10f-79aa4f70f408
@@ -141,6 +225,22 @@ begin
 	
 	Y = selectColumns(train, topCorColumns[1:1])
 	X = selectColumns(train, topCorColumns[2:4])
+end
+
+# ╔═╡ 27c9cac8-0f2e-466c-b97f-baa1cc7df5a1
+begin
+	# build linear regression model
+	
+	function buildLinearRegressionModel(X, Y)
+	    # get number of rows
+	    m = size(X)[1]
+	    # add column of ones to X
+	    X = hcat(ones(m,1), X)
+	    # get theta
+	    theta = inv(X' * X) * X' * Y
+	    return theta
+	end
+	model = buildLinearRegressionModel(X, Y)
 end
 
 # ╔═╡ 4d3671a2-d86d-4879-a019-6647d838b2ce
@@ -206,13 +306,19 @@ begin
 	    return R2
 	end 
 	getR2Accuracy(Y, Yp)
-	
 end
 
 # ╔═╡ 564d98c8-04bf-4f5e-90a7-68e1db06c3ab
 # selet numeric columns only
-numericCols = [:LotArea, :OverallQual, :OverallCond, :YearBuilt, :YearRemodAdd, :MasVnrArea, :BsmtFinSF1, :BsmtFinSF2, :BsmtUnfSF, :TotalBsmtSF, :1stFlrSF, :2ndFlrSF, :LowQualFinSF, :GrLivArea, :BsmtFullBath, :BsmtHalfBath, :FullBath, :HalfBath, :BedroomAbvGr, :KitchenAbvGr, :TotRmsAbvGrd, :Fireplaces, :GarageYrBlt, :GarageCars, :GarageArea, :WoodDeckSF, :OpenPorchSF, :EnclosedPorch, :3SsnPorch, :ScreenPorch, :PoolArea, :MiscVal, :MoSold, :YrSold]
+#numericCols = [:LotArea, :OverallQual, :OverallCond, :YearBuilt, :YearRemodAdd, #:MasVnrArea, :BsmtFinSF1, :BsmtFinSF2, :BsmtUnfSF, :TotalBsmtSF, :1stFlrSF, #:2ndFlrSF, :LowQualFinSF, :GrLivArea, :BsmtFullBath, :BsmtHalfBath, :FullBath, #:HalfBath, :BedroomAbvGr, :KitchenAbvGr, :TotRmsAbvGrd, :Fireplaces, :GarageYrBlt, #:GarageCars, :GarageArea, :WoodDeckSF, :OpenPorchSF, :EnclosedPorch, :3SsnPorch, #:ScreenPorch, :PoolArea, :MiscVal, :MoSold, :YrSold]
 
+
+# ╔═╡ 3d04dc09-ab77-4d11-8c16-9b04742b54d0
+begin
+	# plot histogram of predicted price
+	histogram(Yp, label="Predicted Price")
+	
+end
 
 # ╔═╡ bfa74588-02a5-4157-b1c5-9fb46044a3d9
 # Problem Set
@@ -220,112 +326,15 @@ numericCols = [:LotArea, :OverallQual, :OverallCond, :YearBuilt, :YearRemodAdd, 
 # 2. Submit your predictions 
 
 
-test = CSV.read("test.csv", DataFrame)
+#test = CSV.read("test.csv", DataFrame)
 
-
-# ╔═╡ 898f7277-c933-4c7d-9ff8-185f75a2c158
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-sns.pairplot(numeric[cols], height=2.5)
-plt.show()
-
-# ╔═╡ 962c702a-0b23-47de-81ff-4b4cf5ba972e
-# usefule functions 
-names(train)[33]
-sum(train[:,33] )
-# drop problem column
-select!(train, Not(:PoolArea))
-size(train)
-
-# ╔═╡ 27c9cac8-0f2e-466c-b97f-baa1cc7df5a1
-begin
-	# build linear regression model
-	using Statistics
-	using LinearAlgebra
-	function buildLinearRegressionModel(X, Y)
-	    # get number of rows
-	    m = size(X)[1]
-	    # add column of ones to X
-	    X = hcat(ones(m,1), X)
-	    # get theta
-	    theta = inv(X' * X) * X' * Y
-	    return theta
-	end
-	model = buildLinearRegressionModel(X, Y)
-end
-
-# ╔═╡ 177fe39a-0a5e-4863-9a98-97253dae4dad
-begin
-	# House Sales Data, ETL, Build Predictive Model(linear regression), 
-	# Assess Model, Deploy Model. Training data  'house100Data.csv'
-	# R^2 = 0.75  Price explained by the indep. variables
-	import Pkg; 
-	Pkg.add("DataFrames")
-	Pkg.add("Missings")
-	Pkg.add("Plots")
-	Pkg.add("CSV")
-	using DataFrames
-	using CSV
-	using Missings
-	using Plots
-end
-
-# ╔═╡ c02fd9e8-98e5-4f0e-9d49-d6bb216dcf55
-begin
-	# Build correlation matrix
-	using Statistics
-	cor(Matrix(train))
-	# Plot heatmap of correlation matrix
-	heatmap(cor(Matrix(train)))
-end
-
-# ╔═╡ d57a171a-839c-4a7d-85aa-0ccfa12ff4ec
-begin
-	train = CSV.read("house100Data.csv", DataFrame)
-	size(train)
-end
-
-# ╔═╡ 3d04dc09-ab77-4d11-8c16-9b04742b54d0
-begin
-	# plot histogram of predicted price
-	using Plots
-	histogram(Yp, label="Predicted Price")
-	
-end
-
-# ╔═╡ 52ed110a-f713-4304-8396-956feb699a4e
-begin
-	function dropColumnsWithMissingValues(df)
-	    missingColumns = getListOfMissingColumns(df)
-	    for i in 1:length(missingColumns)
-	        select!(df, Not(Symbol(missingColumns[i])))
-	    end
-	    return df
-	end
-	train = dropColumnsWithMissingValues(train)
-	size(train)
-end
-
-# ╔═╡ 72d6fb52-abfa-40cc-9610-95343d3231bd
-begin
-	# drop non numeric columns
-	function dropNonNumericColumns(df)
-	    nonNumericColumns = getNonNumericColumns(df)
-	    for i in 1:length(nonNumericColumns)
-	        select!(df, Not(Symbol(nonNumericColumns[i])))
-	    end
-	    return df
-	end
-	train = dropNonNumericColumns(train)
-	size(train)
-	
-end
 
 # ╔═╡ Cell order:
 # ╟─ccd33cef-a4f3-43c3-aacc-f0326d07be00
+# ╠═94802ec1-6e6b-4608-bc1e-cb959868baf5
+# ╠═1df4c152-9827-4511-a778-12b16d24f1cb
 # ╠═177fe39a-0a5e-4863-9a98-97253dae4dad
+# ╠═ed8f6d88-2dc7-483e-ace6-321b37ce661d
 # ╠═d57a171a-839c-4a7d-85aa-0ccfa12ff4ec
 # ╠═66013ac0-97ff-45d5-a704-5467853749b5
 # ╠═671944c8-09b7-4a44-a1eb-988d4b23afbd
@@ -337,7 +346,7 @@ end
 # ╠═72d6fb52-abfa-40cc-9610-95343d3231bd
 # ╠═f16533f2-30fb-43fc-8ee7-cccc64ce6bbb
 # ╠═c02fd9e8-98e5-4f0e-9d49-d6bb216dcf55
-# ╠═dd826df7-6a8a-4ae2-9b6a-4a2b2eebff3a
+# ╟─dd826df7-6a8a-4ae2-9b6a-4a2b2eebff3a
 # ╠═713b4fe2-7acc-4e44-b10f-79aa4f70f408
 # ╠═18ee727d-edef-4776-94d2-69c457dc199b
 # ╠═27c9cac8-0f2e-466c-b97f-baa1cc7df5a1
@@ -348,5 +357,3 @@ end
 # ╠═564d98c8-04bf-4f5e-90a7-68e1db06c3ab
 # ╠═3d04dc09-ab77-4d11-8c16-9b04742b54d0
 # ╠═bfa74588-02a5-4157-b1c5-9fb46044a3d9
-# ╠═898f7277-c933-4c7d-9ff8-185f75a2c158
-# ╠═962c702a-0b23-47de-81ff-4b4cf5ba972e
